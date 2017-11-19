1399dee7e7e4718794c7ba21a460f84f890b4d06
Nikos Monios <moniosni@gmail.com>
2017-11-19 16:22:53 +0200

    updated TEWeatherShield.h for the Nucleo board

--- MSP432/TEWeatherShield.h
+++ MSP432/TEWeatherShield.h
@@ -1,14 +1,8 @@
-/*#if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif*/
+#include "mbed.h"
 
-#include "Energia.h"
-
-#include "tsys01.h"
-#include "tsd305.h"
-#include "ms8607.h"
+//#include "tsys01.h"
+//#include "tsd305.h"
+//#include "ms8607.h"
 #include "ms5637.h"
 #include "htu21d.h"
 
@@ -17,20 +11,20 @@ enum TEWeatherShield_Sensor
 {
   Sensor_HTU21D,
   Sensor_MS5637,
-  Sensor_MS8607,
-  Sensor_TSYS01,
-  Sensor_TSD305,
+  //Sensor_MS8607,
+  //Sensor_TSYS01,
+  //Sensor_TSD305,
   Sensor_NONE
 };
 
 class TEWeatherShield 
 {
   public:
-    htu21d HTU21D;
-    ms5637 MS5637;
-    ms8607 MS8607;
-    tsys01 TSYS01;
-    tsd305 TSD305;
+    HTU21D HTU21D; //create the htu21d object
+    MS5637 MS5637;
+    //ms8607 MS8607;
+    //tsys01 TSYS01;
+    //tsd305 TSD305;
   
     TEWeatherShield();
   
@@ -38,8 +32,7 @@ class TEWeatherShield
     void selectSensor(enum TEWeatherShield_Sensor sensor);
     void selectHTU21D();
     void selectMS5637();
-    void selectMS8607();
-    void selectTSYS01();
-    void selectTSD305();
+    //void selectMS8607();
+    //void selectTSYS01();
+    //void selectTSD305();
 };
-
