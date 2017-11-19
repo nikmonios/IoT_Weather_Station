f953b08f669144dddc904b8e8301064ef5d9c8e7
Nikos Monios <moniosni@gmail.com>
2017-11-19 20:06:44 +0200

    updated TEWeatherShield.h for Nucleo

--- Nucleo/TEWeatherShield.h
+++ Nucleo/TEWeatherShield.h
@@ -2,7 +2,7 @@
 
 //#include "tsys01.h"
 //#include "tsd305.h"
-//#include "ms8607.h"
+#include "ms8607.h"
 #include "ms5637.h"
 #include "htu21d.h"
 
@@ -11,7 +11,7 @@ enum TEWeatherShield_Sensor
 {
   Sensor_HTU21D,
   Sensor_MS5637,
-  //Sensor_MS8607,
+  Sensor_MS8607,
   //Sensor_TSYS01,
   //Sensor_TSD305,
   Sensor_NONE
@@ -22,7 +22,7 @@ class TEWeatherShield
   public:
     HTU21D HTU21D; //create the htu21d object
     MS5637 MS5637;
-    //ms8607 MS8607;
+    MS8607 MS8607;
     //tsys01 TSYS01;
     //tsd305 TSD305;
   
@@ -32,7 +32,7 @@ class TEWeatherShield
     void selectSensor(enum TEWeatherShield_Sensor sensor);
     void selectHTU21D();
     void selectMS5637();
-    //void selectMS8607();
+    void selectMS8607();
     //void selectTSYS01();
     //void selectTSD305();
 };
