84545bd40e8c4ffc81fe6a850cda426bd978e295
Nikos Monios <moniosni@gmail.com>
2017-11-22 21:24:55 +0200

    updated with three modules

--- Nucleo/main.cpp
+++ Nucleo/main.cpp
@@ -1,176 +1,60 @@
+/**********************
+* TO DO:
+* add more sensors
+* 
+***********************/
+
 #include "TEWeatherShield.h"
 
-static TEWeatherShield weatherShield;
+Serial pc(USBTX,USBRX);//UART tx, rx (to be sent to PC for logging, and later to esp8266
+static TEWeatherShield weatherShield; //load the shield
 
-void setup() 
+
+int main(void)
 {
-  Serial.begin(9600);
-  Serial.println("TE Connectivity Sensor Solutions");
-  Serial.println("==== Arduino Weather Shield ===="); 
-  Serial.println();
-
-  weatherShield.begin();
-}
-
-void measure_htu21d()
-{
-  float temperature;
-  float humidity;
-  
-  Serial.println("============ HTU21D ============");    
-  weatherShield.selectHTU21D();
-
-  if (weatherShield.HTU21D.is_connected()) 
-  {
-    weatherShield.HTU21D.set_i2c_master_mode(htu21_i2c_no_hold);       
-    weatherShield.HTU21D.read_temperature_and_relative_humidity(&temperature, &humidity); 
     
-    Serial.print("- Temperature : ");
-    Serial.print(temperature, 1);
-    Serial.print((char)176);
-    Serial.println("C");
+    float htu21d_Temperature = 0; //variable for HTU21D temperature in C
+    int htu21d_Humidity = 0; //variable for HTU21D humidity
+    double ms5637_Temp = 0, ms5637_Press = 0; // MS5367 temperature and pressure variables
+    float ms8607_temperature = 0;
+    float ms8607_pressure = 0;
+    float ms8607_humidity = 0;
     
-    Serial.print("- Humidity : ");
-    Serial.print(humidity, 1);
-    Serial.println(" %RH");
-    Serial.println();
-  }
-  else 
-  {
-    Serial.println("HTU21D is disconnected");
-  }
-}
-
-void measure_ms5637() 
-{
-  float temperature;
-  float pressure;
-  
-  Serial.println("============ MS5637 ============");    
-  weatherShield.selectMS5637();
-
-  if (weatherShield.MS5637.is_connected()) 
-  {    
-    weatherShield.MS5637.read_temperature_and_pressure(&temperature, &pressure);
+    pc.baud(9600); //set UART baud rate
+    weatherShield.begin(); //begin the shield
     
-    Serial.print("- Temperature : ");
-    Serial.print(temperature, 1);
-    Serial.print((char)176);
-    Serial.println("C");
+    pc.printf("---- weather station is starting ----\n");
+    wait(1);
     
-    Serial.print("- Pressure : ");
-    Serial.print(pressure, 1);
-    Serial.println (" hPa");
-    Serial.println();
-  }
-  else 
-  {
-    Serial.println("MS5637 is disconnected");
-  }
-}
-
-void measure_ms8607() 
-{
-  float temperature;
-  float pressure;
-  float humidity;
-  
-  Serial.println("============ MS8607 ============");    
-  weatherShield.selectMS8607();
-
-  if (weatherShield.MS8607.is_connected()) 
-  {    
-    weatherShield.MS8607.set_humidity_i2c_master_mode(ms8607_i2c_hold); 
-    weatherShield.MS8607.read_temperature_pressure_humidity( &temperature, &pressure, &humidity);
-    
-    Serial.print("- Tempeature : ");
-    Serial.print(temperature);
-    Serial.print((char)176);
-    Serial.println("C");
-    
-    Serial.print("- Pressure : ");
-    Serial.print(pressure);
-    Serial.println(" hPa");
-    
-    Serial.print("- Humidity : ");
-    Serial.print(humidity);
-    Serial.println("%RH");
-    Serial.println();
-  }
-  else 
-  {
-    Serial.println("MS8607 is disconnected");
-  }
-}
-
-void measure_tsys01() 
-{
-  float temperature;
-  
-  Serial.println("============ TSYS01 ============");    
-  weatherShield.selectTSYS01();
-
-  if (weatherShield.TSYS01.is_connected()) 
-  {    
-    weatherShield.TSYS01.read_temperature(&temperature);
-    
-    Serial.print("- Temperature : ");
-    Serial.print(temperature, 1);
-    Serial.print((char)176);
-    Serial.println("C");
-    Serial.println();
-  }
-  else 
-  {
-    Serial.println("TSYS01 is disconnected");
-  }
-}
-
-void measure_tsd305()
-{
-  float temperature;
-  float object_temperature;
-  
-  Serial.println("============ TSD305 ============");    
-  weatherShield.selectTSD305();
-
-  if (weatherShield.TSD305.is_connected())
-  {    
-    weatherShield.TSD305.read_temperature_and_object_temperature(&temperature, &object_temperature);
-    
-    Serial.print("- Temperature : ");
-    Serial.print(temperature, 1);
-    Serial.print((char)176);
-    Serial.println("C");
-    
-    Serial.print("- Object Temperature : ");
-    Serial.print(object_temperature,1);
-    Serial.print((char)176);
-    Serial.println("C");
-    Serial.println();
-  }
-  else 
-  {
-    Serial.println("TSD305 is disconnected");
-  }
-}
-
-void loop() 
-{    
-  int timer = 0; //var to create 15 minutes delay
-   
-  measure_htu21d();
-  measure_ms5637();
-  measure_ms8607();
-  measure_tsys01();
-  measure_tsd305();
-  
-  Serial.println("================================");
-  Serial.println();
-
-  for(timer = 0; timer <= 180; timer++) //wait for 15 minutes (180 * 5 seconds delay = 900 seconds = 15 minutes
-  {
-    delay(5000); //wait for 5 seconds
-  }
-  timer = 0; //reset counter
+    while(1)
+    {
+        if (weatherShield.HTU21D.is_connected()) //if we have connected to the sensor
+        {
+                htu21d_Temperature = weatherShield.HTU21D.sample_ctemp(); //read the temperature value
+                htu21d_Humidity = weatherShield.HTU21D.sample_humid(); //read the humidity value
+                
+                pc.printf("temperatue from HTU21D is: %f \r\n",htu21d_Temperature); //print Temperature
+                pc.printf("humidity from HTU21D is : %d \r\n",htu21d_Humidity); //prin Humidity
+                
+                wait(5); //wait 5 seconds
+                
+                ms5637_Temp = weatherShield.MS5637.calcTemp(); //read ms5637 temperature
+                ms5637_Press = weatherShield.MS5637.calcPressure(); //read ms5637 pressure
+                
+                pc.printf("temperatue from MS5637 is: %f \r\n",ms5637_Temp); //print Temperature
+                pc.printf("pressure from MS5637 is : %f \r\n",ms5637_Press); //prin Humidity
+                
+                
+                wait(5); //wait 5 seconds
+                
+                weatherShield.MS8607.set_humidity_i2c_master_mode(ms8607_i2c_hold); 
+                weatherShield.MS8607.read_temperature_pressure_humidity( &ms8607_temperature, &ms8607_pressure, &ms8607_humidity);
+                
+                pc.printf("temperatue from MS8607 is: %f \r\n",ms8607_temperature); //print Temperature
+                pc.printf("pressure from MS8607 is : %f \r\n",ms8607_pressure); //prin Pressure
+                pc.printf("humidity from MS8607 is : %f \r\n",ms8607_humidity); //prin Humidity
+                
+                wait(10); //wait 10 seconds
+        }
+    }
 }
